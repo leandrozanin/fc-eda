1b3f1c0fa49e3ca78a5b408c15c888e10e59332a
Leandro Zanin <sr.leandrozanin@gmail.com>
2024-05-13 18:44:05 -0300

    feat: first

---
+++ ms-wallet-balance/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+migrate -path /app/internal/database/migrations -database "mysql://root:root@tcp(wallet-balance-mysql:3308)/wallet?charset=utf8&parseTime=True&loc=Local" up \
+& go run ./cmd/walletbalance/main \
+& tail -f /dev/null
